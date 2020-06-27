2eb9a001edf616b56874f41205ac93543be89d55
zeriot <67459657+zeriot@users.noreply.github.com>
2020-06-27 11:39:39 +0000

    Remove duplicated script

--- misc/cert_gen.sh
+++ misc/cert_gen.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Generate a self signed certificate
-umask 277
-echo "This script should be run on the server"
+umask 077
+# Parameters
 S="${1:-server}"
-openssl ecparam -genkey -name prime256v1 -out "${S}.key"
+A="${2:-ecc}"
+
+if [ "${A}" == "rsa" ]; then
+    openssl genrsa -out "${S}.key" 4096;
+else
+    openssl ecparam -genkey -name prime256v1 -out "${S}.key"
+fi
 openssl req -new -sha256 -key "${S}.key" -out "${S}.csr"
 openssl req -x509 -sha256 -days 730 -key "${S}.key" -in "${S}.csr" -out "${S}.crt"
 rm -f "${S}.csr"
 cat "${S}.key" "${S}.crt" > "${S}.pem"
-echo "Then copy ${S}.crt to the client"
+echo "Then copy ${S}.crt to the other side"
